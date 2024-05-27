72821494eb06e173495057acbd773d278a5ceced
SyedTaybain <104344917+SyedTaybain@users.noreply.github.com>
2024-05-27 09:45:34 +0200

    ui updates

--- lib/pages/chat_detail_page.dart
+++ lib/pages/chat_detail_page.dart
@@ -40,7 +40,6 @@ class _chatDetailState extends State<chatDetail> {
           Expanded(child: totalChat()),
           Container(
             alignment: Alignment.bottomCenter,
-
             child: Padding(
               padding:  EdgeInsets.all(20.0),
               child: Row(
