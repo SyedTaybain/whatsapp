272034dfb8cc6d2b8d2f69a2c97255595b9d688f
SyedTaybain <104344917+SyedTaybain@users.noreply.github.com>
2024-06-08 09:12:06 +0200

    ui updates

--- lib/pages/total_chats.dart
+++ lib/pages/total_chats.dart
@@ -33,9 +33,7 @@ class _totalChatState extends State<totalChat> {
 
 
           margin: const BubbleEdges.only(top: 10),
-
           alignment: Alignment.topRight,
-
           nip: BubbleNip.rightTop,
           color: const Color.fromRGBO(225, 255, 199, 1),
           child: const Text('Hello, World!', textAlign: TextAlign.right),
